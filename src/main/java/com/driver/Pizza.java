23173f434cb24d7628fbafca1713618ab7832512
Vinay Vastrakar <Vinayvastrakar13@gmail.com>
2023-05-12 15:02:39 +0530

    Initial Commit

--- src/main/java/com/driver/Pizza.java
+++ src/main/java/com/driver/Pizza.java
@@ -2,87 +2,78 @@ package com.driver;
 
 public class Pizza {
 
-    private int price;  //final price
-    private int basePrice;
-    private boolean isVeg;
+    private int price;
+    private Boolean isVeg;
     private String bill;
-    private boolean isCheeseAdded;
-    private boolean isExtraTopping;
-    private boolean isTakeAway;
-    private int cheesePrice = 80;
 
-    private int vegBasePrice = 300;
-    private int nonVegBasePrice = 400;
-    private int vegToppingPrice = 70;
-    private int nonVegToppingPrice = 120;
-    private int takeAwayPrice = 20;
-    private int toppingPrice;
+    public boolean isChess = false;
+    public boolean addTopp = false;
 
-    public Pizza(boolean isVeg) {
+    public boolean beg = false;
+    public int getP(){
+        return price;
+    }
+    public void SetP(int price){
+        this.price += price;
+    }
+    public String getB(){
+        return bill;
+    }
+    public void SetB(String bill){
+        this.bill += bill;
+    }
+    public boolean getV(){
+        return isVeg;
+    }
+
+
+    public Pizza(Boolean isVeg){
         this.isVeg = isVeg;
-        this.bill=bill;
-        basePrice = getBasePrice();
-        price = basePrice;
-        isCheeseAdded = false;
-        isExtraTopping=false;
-        isTakeAway=false;
-
+        // your code goes here
+        if(isVeg == true){
+            bill = "Base Price Of The Pizza: 300\n";
+            price = 300;
+        }else{
+            bill = "Base Price Of The Pizza: 400\n";
+            price = 400;
+        }
     }
-
-    private int getBasePrice() {
-        return price = isVeg ? vegBasePrice : nonVegBasePrice;
-    }
-
-    public int getPrice() {
-
+    public int getPrice(){
         return this.price;
     }
 
-    public void addExtraCheese() {
-        // your code goes here
-        if (isCheeseAdded) {
-            isCheeseAdded = true;
-            price += cheesePrice;
+    public void addExtraCheese(){
+        if(!isChess) {
+            SetB("Extra Cheese Added: 80\n");
+            SetP(80);
+            isChess = true;
         }
     }
 
-    public void addExtraToppings() {
-        // your code goes here
-        if (!isExtraTopping) {
-            this.isExtraTopping = true;
-            if (isVeg) {
-                toppingPrice = vegToppingPrice;
-                price += vegToppingPrice;
-            } else
-                toppingPrice = nonVegToppingPrice;
-            price += nonVegToppingPrice;
+    public void addExtraToppings(){
+        if(!addTopp) {
+            if (getV()) {
+                SetB("Extra Toppings Added: 70\n");
+                SetP(70);
+            } else {
+                SetB("Extra Toppings Added: 120\n");
+                SetP(120);
+            }
+            addTopp = true;
         }
     }
 
-    public void addTakeaway(){
-        // your code goes here
-        if (!isTakeAway) {
-            isTakeAway = true;
-            price += takeAwayPrice;
+    public void addTakeaway() {
+        if(!beg) {
+            SetB("Paperbag Added: 20\n");
+            SetP(20);
+            beg = true;
         }
     }
 
-    public String getBill() {
+    public String getBill(){
         // your code goes here
-        String var=" ";
-
-        var += "Base Price Of The Pizza: " + basePrice + "\n";
-        if (isCheeseAdded) {
-            var += "ExtraCheeseAdded: " + cheesePrice + "\n";
-        }
-        if (isExtraTopping) {
-            var += "ExtraToppingAdded: " + toppingPrice + "\n";
-        }
-        if (isTakeAway) {
-            var += "Extra paperbag price: " + takeAwayPrice + "\n";
-        }
-        var += "Total Price: " + price + "\n";
-        this.bill=var;
+        bill+="Total Price: "+price+"\n";
         return this.bill;
     }
 }
